733bfc5a4f64f874deee0db70d31134f053d21ad
bi003121 <tiago.costa@bancointer.com.br>
2022-08-07 20:52:35 -0300

    corrections pointed out in the review

--- optimizing-public-transportation/producers/models/turnstile.py
+++ optimizing-public-transportation/producers/models/turnstile.py
@@ -21,10 +21,10 @@ class Turnstile(Producer):
         """Create the Turnstile"""
         station_name = (
             station.name.lower()
-                .replace("/", "_and_")
-                .replace(" ", "_")
-                .replace("-", "_")
-                .replace("'", "")
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
         )
 
         topic_name = "com.transitchicago.station.turnstiles"
@@ -32,7 +32,7 @@ class Turnstile(Producer):
             f"{topic_name}",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
-            num_partitions=3,
+            num_partitions=1,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -40,17 +40,13 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        if num_entries == 0:
-            logger.info("turnstile kafka integration incomplete. num entries 0 - skipping")
-
-        while num_entries > 0:
+        for _ in range(num_entries):
             self.producer.produce(
                 topic=self.topic_name,
-                key={"timestamp": int(timestamp.timestamp() * 1000)},
+                key={"timestamp": self.time_millis()},
                 value={
                     "station_id": int(self.station.station_id),
                     "station_name": self.station.name,
-                    "line": str(self.station.color),
-                }
+                    "line": str(self.station.color.name),
+                },
             )
-            num_entries = --num_entries
